67b14837534ae82692db48591eabf681413caa19
matihkcs <83300407+matihkcs@users.noreply.github.com>
2021-06-02 22:28:29 -0300

    ejercicio de primalidad

---
+++ tp4ej9.py
@@ -0,0 +1,31 @@
+################
+# Matias Scandizzo - @matihkcs
+# Plantilla de ejercicio Numero 9 Numeros primos
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def es_primo(numero):
+    
+    contador = 0
+    
+    for num in range(numero):
+        if numero % (num+1) != 0:
+            continue
+        else:
+            contador +=1
+    if contador == 2:
+        print("es primo")
+    else:
+        print("no es primo")
+            
+
+
+numero = int(input("escribe un numero: "))
+if es_primo(numero):
+    print("es primo")
+else:
+    print("no es primo")
+    
+        
+if __name__ == "__main__":
+    es_primo(numero)
